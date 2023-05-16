9dcae596a08087d84cb8b790f64739e29b124d3b
Enzo Pedro Bonacina <epbonacina@gmail.com>
2023-05-16 10:34:25 -0300

    Adiciona exemplos sobre c-like enums.

---
+++ c_like_enums/target/debug/c_like_enums.d
@@ -0,0 +1,1 @@
+/home/enzo/studies/learning_rust/c_like_enums/target/debug/c_like_enums: /home/enzo/studies/learning_rust/c_like_enums/src/main.rs
---
+++ c_like_enums/target/debug/deps/c_like_enums-8c79fa0815fb108b.d
@@ -0,0 +1,5 @@
+/home/enzo/studies/learning_rust/c_like_enums/target/debug/deps/c_like_enums-8c79fa0815fb108b: src/main.rs
+
+/home/enzo/studies/learning_rust/c_like_enums/target/debug/deps/c_like_enums-8c79fa0815fb108b.d: src/main.rs
+
+src/main.rs:
