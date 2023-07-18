1cfb90a4f721cccfef28985b243b4aa7ba9cb8d6
Andre Zunino <neyzunino@gmail.com>
2023-07-18 15:58:36 -0300

    Adicionada função de transformação em versão com impl trait e generics.

--- generics-polymorphism/src/main.rs
+++ generics-polymorphism/src/main.rs
@@ -1,5 +1,6 @@
 trait Named {
     fn get_name(&self) -> String;
+    fn update_name(&mut self, new_name: &str);
 }
 
 struct Foo {
@@ -14,57 +15,84 @@ impl Named for Foo {
     fn get_name(&self) -> String {
         return format!("Foo {}", self.name);
     }
+    fn update_name(&mut self, new_name: &str) {
+        self.name = new_name.to_string();
+    }
 }
 
 impl Named for Bar {
     fn get_name(&self) -> String {
         return format!("Bar {}", self.name);
     }
+    fn update_name(&mut self, new_name: &str) {
+        self.name = new_name.to_string();
+    }
 }
 
 // fn handle_named_object<T>(named_object: &T)
 // where T: Named {
-fn handle_named_object<T: Named>(named_object: &T) {
+fn print_named_object<T: Named>(named_object: &T) {
     println!("[handle_named_object] {}", named_object.get_name());
 }
 
-fn handle_named_objects<T: Named>(named_objects: &[&T]) {
+fn print_named_objects<T: Named>(named_objects: &[&T]) {
     for (i, named) in named_objects.iter().enumerate() {
         println!("{}. {}", i, named.get_name());
     }
 }
 
-fn transform_name(named_object: &impl Named) -> impl Named {
+fn transform_name(named_object: &mut impl Named) {
     let parts: Vec<String> = named_object.get_name()
         .split_ascii_whitespace()
         .map(|part| part.to_string())
         .collect();
-    let transformed = parts.get(1).unwrap().to_uppercase();
-    let determinant = named_object.get_name().chars().next().unwrap(); 
-    match determinant {
-        'F' => return Foo { name: transformed },
-        _ => return Foo { name: "Unknown".to_string() }
-    }
+    named_object.update_name(&parts.get(1).unwrap().to_uppercase());
+}
+
+fn generic_transform_name<T: Named>(named_object: &mut T) {
+    let parts: Vec<String> = named_object.get_name()
+        .split_ascii_whitespace()
+        .map(|part| part.to_string())
+        .collect();
+    named_object.update_name(&parts.get(1).unwrap().to_uppercase());
 }
 
 fn main() {
-    let foo_1 = Foo { name: "Foolish".to_string() };
-    let foo_2 = Foo { name: "Foorish".to_string() };
-    let bar_1 = Bar { name: "Barring".to_string() };
+    let mut foo_1 = Foo { name: "Foolish".to_string() };
+    let mut foo_2 = Foo { name: "Foorish".to_string() };
+    let mut bar_1 = Bar { name: "Barring".to_string() };
 
-    handle_named_object(&foo_1);
-    handle_named_object(&bar_1);
+    print_named_object(&foo_1);
+    print_named_object(&bar_1);
 
     println!();
 
     // let named_objects: Vec<&dyn Named> = vec![&foo, &bar];
-    handle_named_objects(&[&foo_1, &foo_2]);
-    handle_named_objects(&[&bar_1]);
+    print_named_objects(&[&foo_1, &foo_2]);
+    print_named_objects(&[&bar_1]);
 
     println!();
 
-    let transformed_foo_1 = transform_name(&foo_1);
-    let transformed_bar_1 = transform_name(&bar_1);
-    println!("Transformed foo_1: {}", transformed_foo_1.get_name());
-    println!("Transformed bar_1: {}", transformed_bar_1.get_name());
+    transform_name(&mut foo_1);
+    transform_name(&mut foo_2);
+    transform_name(&mut bar_1);
+    println!("Transformed foo_1: {}", foo_1.get_name());
+    println!("Transformed foo_2: {}", foo_2.get_name());
+    println!("Transformed bar_1: {}", bar_1.get_name());
+
+    println!();
+
+    let mut foo_3 = Foo { name: "Fookish".to_string() };
+    let mut bar_2 = Bar { name: "Barting".to_string() };
+
+    print_named_object(&foo_3);
+    print_named_object(&bar_2);
+
+    generic_transform_name(&mut foo_3);
+    generic_transform_name(&mut bar_2);
+
+    println!();
+
+    print_named_object(&foo_3);
+    print_named_object(&bar_2);
 }
