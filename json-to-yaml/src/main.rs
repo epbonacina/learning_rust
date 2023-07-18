17100559809500a3040aa2dedc5791adf6b95833
Andre Zunino <neyzunino@gmail.com>
2023-07-18 17:15:30 -0300

    Implementação de um iterador sobre coleções de Game.

--- json-to-yaml/src/main.rs
+++ json-to-yaml/src/main.rs
@@ -10,6 +10,30 @@ struct Game {
     developer: String,
 }
 
+struct GameIterator<'a> {
+    games: &'a[Game],
+    idx: usize,
+}
+
+impl<'a> GameIterator<'a> {
+    fn new(games: &'a[Game]) -> Self {
+        GameIterator {games, idx: 0}
+    }
+}
+
+impl<'a> Iterator for GameIterator<'a> {
+    type Item = &'a Game;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx < self.games.len() {
+            let game = self.games.get(self.idx);
+            self.idx += 1;
+            return game;
+        }
+        None
+    }
+}
+
 fn read_games_from_json_file(json_path: &str) -> Result<Vec<Game>, Box<dyn Error>> {
     let json_file = File::open(json_path)?;
     let reader = BufReader::new(json_file);
@@ -25,7 +49,15 @@ fn write_games_to_yaml_file(games: &[Game], yaml_path: &str) -> Result<(), Box<d
     Ok(())
 }
 
+fn check_out_game_iterator(games: &[Game]) {
+    let iter = GameIterator::new(games);
+    for game in iter {
+        println!("{}", game.name);
+    }
+}
+
 fn main() {
     let games = read_games_from_json_file("data/games.json").unwrap();
-    let _ = write_games_to_yaml_file(&games, "data/games.yaml");
+    // let _ = write_games_to_yaml_file(&games, "data/games.yaml");
+    check_out_game_iterator(&games);
 }
